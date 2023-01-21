34a6126084bd82f00b2aa5f3d8d967aeb6199b38
Juliancio Carvalho <juliancio_alves@hotmail.com>
2023-01-21 17:31:27 -0300

    feat: add app1 and app2 projects

---
+++ app2/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div
+      style={{
+        margin: "10px",
+        padding: "10px",
+        textAlign: "center",
+        backgroundColor: "blue",
+        color: "white",
+      }}
+    >
+      <h1>Products</h1>
+    </div>
+  )
+}
+
+export default App
